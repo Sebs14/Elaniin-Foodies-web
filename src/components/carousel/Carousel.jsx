96e1dba75731801b57e0d85942e7f8834d8c30a0
Sebastian <sebastian.flores@elaniin.com>
2023-01-19 17:08:17 -0600

    feat: :sparkles: Carousel changes
    Change carousel so it can be used with strapi instead of local
    
    change the implementation of the carousel to be used with strapi

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,39 +5,12 @@ import {IoIosArrowBack, IoIosArrowForward} from "react-icons/io"
 import LeftSide from "../../assets/left_side.jsx"
 import RightSide from "../../assets/right_side";
 import Ketchup from "../../assets/testimonials_bg_ketchup.png"
-
-
-const sliderData = [
-  {
-    id: 1,
-    title: '"El mejor lugar para degustar en familia y amigos"',
-    text: "es el mejor lugar al que he venido con mi familia, la comida es rica, sirven rápido y te atienden de la mejor manera.",
-  },
-  {
-    id: 2,
-    title: '"El mejor lugar para comer con mi novia"',
-    text: "es el mejor lugar para tener una cena romantica",
-  },
-  {
-    id: 3,
-    title: '"El mejor lugar para ir con mi perro y comer"',
-    text: "es el mejor lugar al que he venido con mi perro, tienen lugares para que el se sienta comodo y pueda jugar con otros perros.",
-  },
-  {
-    id: 4,
-    title: '"El mejor lugar para comer con los panas"',
-    text: "Puedes ver partidos del mundial y te dan cerveza si tu equipo gana",
-  },
-  {
-    id: 5,
-    title: '"Buen lugar para poder ir a disfrutar"',
-    text: "Mi novia me lo recomendo el lugar, y vaya que si saben atender a las personas en este restaurante",
-  },
-];
+import getText from "../../Services/getText";
 
 const Carousel = () => {
+  const [sliderData, setSliderData] = useState([]);
   const [slide, setSlide] = useState(0);
-  const length = sliderData.length;
+  const length = sliderData?.length;
 
   const prevSlide = () => {
     setSlide(slide === length - 1 ? 0 : slide + 1)
@@ -46,24 +19,31 @@ const Carousel = () => {
     setSlide(slide === 0 ? length - 1 : slide - 1)
   }
 
+  const fetchData = async () => {
+    const data = await getText('data-sliders')
+    setSliderData(data)
+  }
 
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
     <div>
       <div id="carousel" className="flex-col max-w-full px-4 py-16 relative flex justify-center items-center lg:h-[75vh] h-[65vh]  ">
         <LeftSide/>
         <RightSide/>
-        {sliderData.map((item, index) => (
+        {sliderData?.map((item, index) => (
           <div key={item.id} className={index === slide ? "opacity-100 flex justify-center" : "opacity-0"}>
             {index === slide && (
               <div className="flex flex-col justify-center w-3/4 z-10">
                 <Titles
                   classes="font-drukTextWide text-center lg:text-4xl md:text-3xl text-xl"
-                  titleText={item.title}
+                  titleText={item.attributes.title}
                 />
                 <Paragraph
                   classes="font-noto text-lg text-black/50 text-center pt-2"
-                  pText={item.text}
+                  pText={item.attributes.subtitle}
                 />
               </div>
             )}
